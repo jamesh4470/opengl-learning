784b47eb4365b3e52783febc972b44cd671331f0
jamesh4470 <jamesh4470@gmail.com>
2026-06-21 18:44:47 -0500

    colors

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
-output:
+output: main.cpp
 	g++ main.cpp glad/src/glad.c \
 		-I glad/include \
 		-I glfw-3.4/include \
 		-Lglfw-3.4/build/src -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl \
 		-o output
+
+run:
+	./output
+
+clean:
+	rm output
--- main.cpp
+++ main.cpp
@@ -2,10 +2,55 @@
 #include "glfw-3.4/include/GLFW/glfw3.h"
 #include <iostream>
 
+// callback function everytime the user resizes the window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    // set the rendering window size relative to the os window size
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main() {
     glfwInit();
+    // set version to opengl 3.3 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // use opengl core profile
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    std::cout << "hello world!" << std::endl;
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test Window", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // initialize glad before any opengl function calls
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // set the callback function for window resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents(); // checks if events are triggered and calls callback functions if so
+    }
+
+    glfwTerminate();
+    return 0;
 }
+
