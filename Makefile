a79b9044668808b78ddd474a49d9ec50e01653b4
jamesh4470 <jamesh4470@gmail.com>
2026-06-21 18:09:07 -0500

    fix makefile error

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@ output:
 	g++ main.cpp glad/src/glad.c \
 		-I glad/include \
 		-I glfw-3.4/include \
-		-L -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl \
+		-Lglfw-3.4/build/src -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl \
 		-o output
--- main.cpp
+++ main.cpp
@@ -3,5 +3,9 @@
 #include <iostream>
 
 int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     std::cout << "hello world!" << std::endl;
 }
